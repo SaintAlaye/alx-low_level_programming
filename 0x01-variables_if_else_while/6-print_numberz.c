e7550b3ccb9450a79d444d5af91a75c321b795b3
SaintAlaye <elitetechydev@gmail.com>
2023-03-17 15:41:14 -0700

    Printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digits;
+	for (digits = 48; digits <=57; digits++)
+		putchar(digits);
+
+	putchar('\n');
+
+	return (0);
+
+}
