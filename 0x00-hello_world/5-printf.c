a5535868972ed31331daef556f6474c951bffefe
SaintAlaye <elitetechydev@gmail.com>
2023-03-16 18:00:40 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints
+ * * exactly with proper grammar, but the outcome is a piece of art,
+ * followed by a new line.
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
