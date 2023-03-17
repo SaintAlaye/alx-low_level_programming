d2df10c87a04072b3d5b4f464077713aa49efa3f
SaintAlaye <elitetechydev@gmail.com>
2023-03-17 11:07:27 -0700

    This program prints alphabets in lowercases using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This program prints alphabets in lower cases'
+ * Return: Always 0 (Success/Correct)
+ */
+int main(void)
+{
+	/* the character to be written*/
+	char lower_Case = 'a';
+
+	/* write the character to stdout */
+	for (lower_Case = 'a'; lower_Case <= 'z'; lower_Case++)
+		putchar(lower_Case);
+
+	return (0);
+}
