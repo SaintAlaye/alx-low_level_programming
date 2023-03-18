25e00cfca7e000c12fd913d9a858d19f3ec39819
SaintAlaye <elitetechydev@gmail.com>
2023-03-17 19:50:43 -0700

    a program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar(num2 / 10 + 48);
 				putchar(num2 % 10 + 48);
 
-				if (num1 != 98 || num2 != 99)
+				if (num1 != 98 && num2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
