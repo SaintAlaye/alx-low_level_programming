f66af9022c1928b72d15cc847c214fee3a52b83f
SaintAlaye <elitetechydev@gmail.com>
2023-04-09 11:13:50 -0700

    argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,15 +2,13 @@
 #include "main.h"
 
 /**
- * main - a function that prints all the
- * arguments passed into it including the 
- * first one
+ * main - a function that prints all the arguments passed into it
  * @argc: number of argument count passed
  * @argv: arrays of string of argument
  * Return 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int count;
 
