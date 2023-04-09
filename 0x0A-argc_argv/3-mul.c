acfe15d8adcd4f6bbf4ca99662ad9737dfbef051
SaintAlaye <elitetechydev@gmail.com>
2023-04-09 07:50:53 -0700

    Argc, Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,8 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
-/**
+/*
  * main - a function that multiply 2 numbers
  * @agrc: number of argument count
  * @argv: arrays of strings of arguments
@@ -11,14 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	(void) argc;
+
+	if (argv[1] && argv[2])
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
+	else
+		printf("Error\n");
+
+	return (1);
 }
