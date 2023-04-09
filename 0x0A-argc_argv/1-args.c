a1eb8860021e0f5c63c5baa3647c4859ecac9ddc
SaintAlaye <elitetechydev@gmail.com>
2023-04-09 11:06:20 -0700

    Argv, Argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - a function that prints 
- * the number of arguments passed into it
+ * main - a function that prints the number of arguments passed into it
  * @argv: number of array of string
- * @agrc: number of arguments passed
+ * @argc: number of arguments passed
  * Return: 0 Success
  */
 
-int main(int argc, char *agrv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 
