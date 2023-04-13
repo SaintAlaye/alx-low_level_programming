c6d3fd5505c5667236b82352c1f2bdbf4532c0fc
SaintAlaye <elitetechydev@gmail.com>
2023-04-13 04:39:00 -0700

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory using malloc.
+ * Description: If malloc fails, terminate process with status 98.
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to malloc'd memory space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	/* checking if P is 0 */
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
